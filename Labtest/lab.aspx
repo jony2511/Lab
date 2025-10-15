efc1585b7e8932122ce7c6e2b7b98dc836750f54
jony2511 <141722597+jony2511@users.noreply.github.com>
2025-10-15 10:42:48 +0600

    Add files via upload
    asp.net files

---
+++ Labtest/lab.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="lab.aspx.cs" Inherits="Grading.lab" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <h3>Grade Management System</h3>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label runat="server" Text="Student ID "></asp:Label>
+            <asp:TextBox runat="server" Placeholder="Enter student ID"></asp:TextBox><br />
+            <asp:Label runat="server" ID="Number" Text="Number "></asp:Label>
+            <asp:TextBox runat="server" ID="mark" Placeholder="Enter mark here"></asp:TextBox><br /><br />
+            <asp:Button runat="server" ID="submit" Text="Submit" Onclick="SubmitBtn" /><br /><br />
+            <p><asp:label runat="server" id="a" Text="Current Number of A+: 0" ></asp:label></p>
+            <p><asp:label runat="server" id="b" Text="Current Number of B+: 0" ></asp:label></p>
+            <p><asp:label runat="server" id="c" Text="Current Number of C+: 0" ></asp:label></p>
+            <p><asp:label runat="server" id="f" Text="Current Number of F: 0" ></asp:label></p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Labtest/lab.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Grading
+{
+    public partial class lab : System.Web.UI.Page
+    {
+        public class GradingSystem
+        {
+            string student;
+            int number;
+            public GradingSystem()
+            {
+                if (HttpContext.Current.Session["a"] == null)
+                    HttpContext.Current.Session["a"] = 0;
+                if (HttpContext.Current.Session["b"] == null)
+                    HttpContext.Current.Session["b"] = 0;
+                if (HttpContext.Current.Session["c"] == null)
+                    HttpContext.Current.Session["c"] = 0;
+                if (HttpContext.Current.Session["f"] == null)
+                    HttpContext.Current.Session["f"] = 0;
+            }
+            public void setGrade(int mark)
+            {
+                if(mark>=80 && mark<=100)
+                     HttpContext.Current.Session["a"] =(int) HttpContext.Current.Session["a"] + 1;
+                else if (mark >= 60 && mark<= 79)
+                     HttpContext.Current.Session["b"] = (int)HttpContext.Current.Session["b"] + 1;
+                if (mark >= 40 && mark<= 59)
+                     HttpContext.Current.Session["c"] = (int)HttpContext.Current.Session["c"] + 1;
+                if (mark >= 0 && mark<= 39)
+                     HttpContext.Current.Session["f"] = (int)HttpContext.Current.Session["f"] + 1;
+                
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+        protected void update()
+        {
+            a.Text = "Current Number of A+: " + HttpContext.Current.Session["a"];
+            b.Text = "Current Number of B+: " + HttpContext.Current.Session["b"];
+            c.Text = "Current Number of C+: " + HttpContext.Current.Session["c"];
+            f.Text = "Current Number of F: " + HttpContext.Current.Session["f"];
+        }
+        protected void SubmitBtn(object sender, EventArgs e)
+        {
+            int marks;
+            if(int.TryParse(mark.Text,out marks))
+                {
+                GradingSystem g=new GradingSystem();
+                g.setGrade(marks);
+                update();
+            }
+        }
+    }
+}
